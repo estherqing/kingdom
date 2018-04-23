8febb019d2b400d66a288dd5ef6267f8c53e57ba
esther-jia <jiafuqing@zafh.com.cn>
2018-04-23 10:16:45 +0800

    ehcache的实现

--- base-web/src/test/java/com/esther/code/UserControllerTest.java
+++ base-web/src/test/java/com/esther/code/UserControllerTest.java
@@ -1,6 +1,21 @@
 package com.esther.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author esther
@@ -9,31 +24,31 @@ import org.junit.Test;
  */
 
 public class UserControllerTest {
-    private String get="http://localhost:8080/user/get";
+    private String get="http://localhost:8086/kingdom/user/get";
 
     @Test
     public void getSignInfoList() throws Exception {
-//        HttpClient httpClient = HttpClients.createDefault();
-//        HttpPost post = new HttpPost(get);
-//        System.out.println("request=" + get);
-//        //post.addHeader("Cookie", "JSESSIONID=" + loginResult.getToken());
-//        List<NameValuePair> params = new ArrayList<NameValuePair>();
-//        params.add(new BasicNameValuePair("id", "1"));
-//
-//        HttpEntity entity = new UrlEncodedFormEntity(params, Charset.defaultCharset());
-//        post.setEntity(entity);
-//        HttpResponse httpResponse = httpClient.execute(post);
-//        HttpEntity respEntity = httpResponse.getEntity();
-//        int code = httpResponse.getStatusLine().getStatusCode();
-//        if (code == 302) {
-//            String redirect = httpResponse.getHeaders("Location")[0].getValue();
-//            HttpGet get1 = new HttpGet(redirect);
-//            httpResponse = httpClient.execute(get1);
-//            respEntity = httpResponse.getEntity();
-//        }
-//        String body = EntityUtils.toString(respEntity);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//
-//        System.out.println("body=" + body);
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost post = new HttpPost(get);
+        System.out.println("request=" + get);
+        //post.addHeader("Cookie", "JSESSIONID=" + loginResult.getToken());
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("id", "1"));
+
+        HttpEntity entity = new UrlEncodedFormEntity(params, Charset.defaultCharset());
+        post.setEntity(entity);
+        HttpResponse httpResponse = httpClient.execute(post);
+        HttpEntity respEntity = httpResponse.getEntity();
+        int code = httpResponse.getStatusLine().getStatusCode();
+        if (code == 302) {
+            String redirect = httpResponse.getHeaders("Location")[0].getValue();
+            HttpGet get1 = new HttpGet(redirect);
+            httpResponse = httpClient.execute(get1);
+            respEntity = httpResponse.getEntity();
+        }
+        String body = EntityUtils.toString(respEntity);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println("body=" + body);
     }
 }
---
+++ base-web/src/test/java/com/esther/code/cache/ehcache/MyEhcache.java
@@ -0,0 +1,53 @@
+package com.esther.code.cache.ehcache;
+
+import com.esther.code.model.User;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+
+/**
+ * @author esther
+ * 2018-04-20 20:01
+ * $DESCRIPTION}
+ */
+
+public class MyEhcache {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1);
+        user.setUserName("rose");
+        user.setUserRealName("rose green");
+        user.setAddress("china");
+        user.setAge(25);
+        user.setPassword("111111");
+        user.setSex("female");
+
+        // 获取缓存管理器
+        // 从resources目录下获取默认ehcache.xml文件
+        CacheManager manager = CacheManager.getInstance();
+        //CacheManager manager= CacheManager.create();
+
+//        从classpath中找寻配置文件并创建:
+//        MyEhcache myEhcache = new MyEhcache();
+//        CacheManager  manager = CacheManager.create(myEhcache.getClass().getResource("/ehcache.xml"));
+
+
+        // 根据配置文件获得Cache实例
+        Cache demo = manager.getCache("demoCache");
+
+        // 清空Cache中的所有元素
+        demo.removeAll();
+        demo.put(new Element("hello", "world"));
+        demo.put(new Element(user.getUserName(), user));
+
+
+        Element e = demo.get(user.getUserName());
+
+        System.out.println(demo.get("hello").getObjectValue());
+        System.out.println(((User) e.getObjectValue()).getUserRealName());
+
+        //卸载缓存管理器，关闭Cache
+        manager.shutdown();
+    }
+}
